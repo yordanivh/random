03eea8a63f94783a65d5485565a4b6c363b352fa
Yordan Halachev <yordan.i.halachev@gmail.com>
2020-01-03 16:05:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "yordanh_free"
+
+    workspaces {
+      name = "random"
+    }
+  }
+}
 resource "random_id" "example" {
 
   byte_length = 8
